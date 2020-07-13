ade9b483cd7572129bb3e61cf0b2958d90d3f074
Avijit Das <avijitdasnoyon@gmail.com>
2020-07-13 19:45:00 +0600

    Write modified logs to a file in modified directory

--- account_log_parser.py
+++ account_log_parser.py
@@ -1,4 +1,7 @@
 with open('./original/accounting.log', 'r') as auth_log:
     read_account_log = auth_log.read()
 
-print(read_account_log.replace("|!|", " "))
+replace_symbol = read_account_log.replace("|!|", " ")
+
+with open("./modified/accounting-mod.log", "w") as parse_logs:
+    parse_logs.write(replace_symbol)
--- auth_log_parser.py
+++ auth_log_parser.py
@@ -1,4 +1,7 @@
 with open('./original/auth.log', 'r') as auth_log:
     read_auth_log = auth_log.read()
 
-print(read_auth_log.replace("|!|", " "))
+replace_symbol = read_auth_log.replace("|!|", " ")
+
+with open("./modified/auth-mod.log", "w") as parse_logs:
+    parse_logs.write(replace_symbol)
